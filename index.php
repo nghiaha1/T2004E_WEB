f5af72e2eb2670549fe7dbf7314b06469428246a
nghiaha1 <64962858+nghiaha1@users.noreply.github.com>
2020-06-04 18:34:11 +0700

    CODE SESSION 1

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
 <?php include_once("home.html");
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Test</title>
-</head>
-<body>
-    <p>con cac</p>
-</body>
-</html>
